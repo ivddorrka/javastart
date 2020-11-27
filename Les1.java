ec18159d199bfebc84b5415d3d12d40f1eff3577
ivddorrka <ivddorrka@gmail.com>
2020-11-27 23:16:09 +0200

    To words in one line

--- Les1.java
+++ Les1.java
@@ -2,8 +2,9 @@ package randomfun;
 
 public class Les1 {
     public static void main(String[] args) {
-        System.out.print("welcome");
         
+System.out.print("welcome");
+System.out.print(" welcome#2");
     }
 
 }
