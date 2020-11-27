259911c552af4cbc4fb54e10f8b84ea2d22525b3
ivddorrka <ivddorrka@gmail.com>
2020-11-28 01:40:54 +0200

    Data types

---
+++ Les21.java
@@ -0,0 +1,18 @@
+package randomfun;
+
+public class Les21 {
+    public static void main(String[] args) {
+    
+        byte bite1 = 12;
+        byte bite2 = 0b1100;
+        byte bite3 = 0xc;
+//        char woh1 = "w";
+        float fl1 = 12.4f;
+        double dbl1 = 12.4;
+        boolean week = true;
+        double answer1 = bite1 + bite2;
+System.out.print(answer1);
+        
+    }
+    
+}
